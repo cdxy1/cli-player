15c5b8f1b1f49836818b352b83b0039fd8518a49
cdxy1 <cdxy@list.ru>
2025-01-28 01:16:16 +0300

    Fix cli

--- player/core/music.py
+++ player/core/music.py
@@ -6,9 +6,4 @@ def play_track(track_path: Path):
     if not isinstance(track_path, Path):
         raise TypeError("Arg is not a path.")
 
-    try:
-        subprocess.run(["mpv", "--vo=tct", "--no-audio-display", track_path])
-    except FileNotFoundError:
-        pass  # File not found
-    except subprocess.CalledProcessError:
-        pass  # Execution error
+    subprocess.run(["mpv", "--no-audio-display", track_path])
--- player/main.py
+++ player/main.py
@@ -19,12 +19,12 @@ def select_menu():
     while True:
         click.clear()
         click.echo(
-            "Select or create playlist:\n 1. Select playlist\n 2. Create playlist"
+            f"{click.style('Select or create playlist:', bold=True)}\n 1. Select playlist\n 2. Create playlist\n"
         )
-        choice = click.prompt("Enter a number", type=int)
+        choice = click.prompt(click.style("Enter a number", bold=True), type=int)
         if choice not in (1, 2):
             click.clear()
-            click.echo("Enter 1 or 2, please!")
+            click.secho("Enter 1 or 2, please!", bold=True, fg="yellow")
             continue
         else:
             return choice
@@ -33,14 +33,16 @@ def select_menu():
 def make_playlist():
     pl_lst = playlists_list()
     while True:
-        name = click.prompt("Enter a name of playlist", type=str)
+        name = click.prompt(
+            click.style("Enter a name of playlist", bold=True), type=str
+        )
         if not isinstance(name, str):
             click.clear()
-            click.echo("Name must be string.")
+            click.secho("Name must be string!", bold=True, fg="yellow")
             continue
         elif name in pl_lst.keys():
             click.clear()
-            click.echo("The playlist name is already exists.")
+            click.secho("The playlist name is already exists.", bold=True, fg="yellow")
             continue
         else:
             make_playlist_dir(name)
@@ -50,9 +52,9 @@ def make_playlist():
 def show_playlists():
     pl_lst = playlists_list()
     click.clear()
-    click.echo("Playlists:\n==========")
+    click.echo(f"{click.style('Playlists', bold=True)}:\n==========")
     for num, key in enumerate(pl_lst.keys()):
-        click.echo(f"{num}. {key.title()}")
+        click.echo(f"{click.style(num, bold=True)}. {key.title()}")
     else:
         click.echo("==========\n")
 
@@ -62,6 +64,7 @@ def show_playlists():
 def playlist_ended(lst, num):
     flag = True
     current_num = num
+    menu = f"{click.style('What next?', bold=True)}:\n1. Next playlist\n2. Retry current playlist\n3. Choose another playlist\n"
 
     click.clear()
 
@@ -69,10 +72,11 @@ def playlist_ended(lst, num):
         for sec in range(10, 0, -1):
             if flag:
                 click.clear()
+                click.secho(menu)
                 click.echo(
-                    "\nWhat next?:\n1. Next playlist\n2. Retry current playlist\n3. Choose another playlist"
+                    f"{click.style('Press', bold=True)} {click.style('enter', fg='yellow')} {click.style('to select option.', bold=True)} S({sec})",
                 )
-                click.echo(f"Press something to select option. S({sec})")
+
                 try:
                     if _ := inputimeout(timeout=1):
                         import sys, termios
@@ -83,10 +87,8 @@ def playlist_ended(lst, num):
                     continue
             else:
                 click.clear()
-                click.echo(
-                    "\nWhat next?:\n1. Next playlist\n2. Retry current playlist\n3. Choose another playlist"
-                )
-                step = click.prompt("Enter a action", type=int)
+                click.echo(menu)
+                step = click.prompt(click.style("Enter a action", bold=True), type=int)
                 break
         else:
             step = 1
@@ -98,7 +100,7 @@ def playlist_ended(lst, num):
                     current_num += 1
                     flag = True
                 except IndexError:
-                    click.echo("It was the last playlist!!!")
+                    click.secho("It was the last playlist!!!", bold=True, fg="yellow")
                     break
             case 2:
                 select_pl_dir(lst, current_num)
@@ -107,23 +109,20 @@ def playlist_ended(lst, num):
                 break
             case _:
                 click.clear()
-                click.echo(
-                    "\nWhat next?:\n1. Next playlist\n2. Retry current playlist\n3. Choose another playlist"
-                )
-                click.echo("Enter a valid action.")
+                click.echo(menu)
+                click.secho("Enter a valid action.", bold=True, fg="yellow")
 
 
 def select_playlist():
     while True:
         pl_lst = show_playlists()
 
-        num = click.prompt("Choose playlist", type=int)
-        print(num)
+        num = click.prompt(click.style("Choose playlist", bold=True), type=int)
         if num > len(pl_lst.keys()) or num < 0:
-            click.echo("Number out of range.")
+            click.secho("Number out of range.", bold=True, fg="yellow")
             continue
         elif not isinstance(num, int):
-            click.echo("Arg must be integer.")
+            click.secho("Arg must be integer.", bold=True, fg="yellow")
             continue
         else:
             select_pl_dir(pl_lst, num)
@@ -132,11 +131,11 @@ def select_playlist():
 
 @cli.command()
 def start():
-    click.echo("Start player")
-
     if not check_main_dir:
         path = click.prompt(
-            "Enter default dir path or leave blank", type=str, default=""
+            click.style("Enter default dir path or leave blank", bold=True),
+            type=str,
+            default="",
         )
         init_main_dir(path)
 
